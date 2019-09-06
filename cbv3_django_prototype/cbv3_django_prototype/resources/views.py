d24597d62ea84d4fa53b3b9f835ed287191e79be
Brylie Christopher Oxley <brylie@amble.fi>
2019-09-06 23:06:48 +0300

    Add Resource viewset

--- cbv3_django_prototype/cbv3_django_prototype/resources/views.py
+++ cbv3_django_prototype/cbv3_django_prototype/resources/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ResourceSerialiazer
+from .models import Resource
 
-# Create your views here.
+
+class ResourceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Resources to be viewed or edited.
+    """
+
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerialiazer
