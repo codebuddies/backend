2e55832646a033429e3b6a6c9f8c476347defb39
Brylie Christopher Oxley <brylie@amble.fi>
2019-09-06 21:58:09 +0300

    Add resources app

--- cbv3_django_prototype/config/settings/base.py
+++ cbv3_django_prototype/config/settings/base.py
@@ -77,7 +77,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "cbv3_django_prototype.users.apps.UsersConfig",
-    # Your stuff: custom apps go here
+    "resources.apps.ResourcesConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
