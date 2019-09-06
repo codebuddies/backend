1432ff4702d3f38313050434fdeb0b51de36f169
Brylie Christopher Oxley <brylie@amble.fi>
2019-09-06 23:07:01 +0300

    Add API URLs

--- cbv3_django_prototype/config/urls.py
+++ cbv3_django_prototype/config/urls.py
@@ -4,8 +4,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from rest_framework import routers
+
+from resources import views as resource_views
+
+router = routers.DefaultRouter()
+router.register(r"resources", resource_views.ResourceViewSet)
 
 urlpatterns = [
+    path("api/", include(router.urls)),
+    path("api-auth", include("rest_framework.urls", namespace="rest_framework")),
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
