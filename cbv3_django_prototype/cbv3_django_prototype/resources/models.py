aa108aaae3434bcf2e6f4051f4f58c673adf8b05
Brylie Christopher Oxley <brylie@amble.fi>
2019-09-06 21:57:52 +0300

    Initial Resource model

--- cbv3_django_prototype/cbv3_django_prototype/resources/models.py
+++ cbv3_django_prototype/cbv3_django_prototype/resources/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+RESOURCE_TYPE_CHOICES = [
+    ("tutorial", "Tutorial"),
+    ("course", "Course"),
+    ("community", "Community"),
+]
+
+
+class Resource(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    resource_type = models.CharField(max_length=255, choices=RESOURCE_TYPE_CHOICES)
+    credit = models.CharField(max_length=255)
+    url = models.URLField()
+    referrer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
