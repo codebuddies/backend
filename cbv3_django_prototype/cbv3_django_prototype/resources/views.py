ea3e21652beeea7945dce9ccdfb1c1762739af0c
Bethany Garcia <bethanymgarcia@gmail.com>
2019-09-18 17:07:11 -0700

    Added generic ModelViewSet for ResoureListView.

--- cbv3_django_prototype/cbv3_django_prototype/resources/views.py
+++ cbv3_django_prototype/cbv3_django_prototype/resources/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Resource
+from django.shortcuts import render, get_object_or_404
+from django.contrib.postgres.fields import JSONField
+from .serializers import ResourceSerializer
+from rest_framework import filters, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ResourceListView(viewsets.ModelViewSet):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['id', '']
