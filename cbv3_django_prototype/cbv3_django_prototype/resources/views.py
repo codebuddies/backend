9a5b68fa38d3c253006608aeae23d110cf06b2f7
Bethany Garcia <bethanymgarcia@gmail.com>
2019-09-24 22:08:20 -0700

    Added media_type, descripton, and tags to search_fields.

--- cbv3_django_prototype/cbv3_django_prototype/resources/views.py
+++ cbv3_django_prototype/cbv3_django_prototype/resources/views.py
@@ -1,14 +1,10 @@
 from .models import Resource
-from django.shortcuts import render, get_object_or_404
-from django.contrib.postgres.fields import JSONField
 from .serializers import ResourceSerializer
 from rest_framework import filters, viewsets
-from rest_framework.response import Response
 
 
-class ResourceListView(viewsets.ModelViewSet):
+class ResourceView(viewsets.ModelViewSet):
     queryset = Resource.objects.all()
     serializer_class = ResourceSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['id', 'tags']
-
+    search_fields = ['id', 'media_type', 'title', 'description', 'tags']
