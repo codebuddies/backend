59a6e9ca01b6a5f44ad8e19439e40b359d0134d2
Bethany Garcia <bethanymgarcia@gmail.com>
2019-09-24 22:11:33 -0700

    Removied commented out detail by id path.

--- cbv3_django_prototype/cbv3_django_prototype/resources/urls.py
+++ cbv3_django_prototype/cbv3_django_prototype/resources/urls.py
@@ -3,10 +3,9 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'resources', views.ResourceListView, basename='resources')
+router.register(r'resources', views.ResourceView, basename='resources')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-    #path('<int:id>/', views.DetailResource.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
