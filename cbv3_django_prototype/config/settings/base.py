1051c3cf24f7e2a7eac96aba81a537689eabb659
Bethany Garcia <bethanymgarcia@gmail.com>
2019-12-19 20:50:23 -0800

    Wired up new userauth app.

--- cbv3_django_prototype/config/settings/base.py
+++ cbv3_django_prototype/config/settings/base.py
@@ -78,6 +78,7 @@ THIRD_PARTY_APPS = [
     "corsheaders",
     "taggit",
     "django_celery_beat",
+    "taggit_serializer"
 ]
 
 LOCAL_APPS = [
@@ -301,7 +302,6 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated', ],
 
-
     'TEST_REQUEST_DEFAULT_FORMAT': ['json', ],
 
     'TEST_REQUEST_RENDERER_CLASSES': [
@@ -317,6 +317,13 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication', ],
+
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny',],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
@@ -336,4 +343,5 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CORS_ALLOW_CREDENTIALS = True
+
 # ------------------------------------------------------------------------------
