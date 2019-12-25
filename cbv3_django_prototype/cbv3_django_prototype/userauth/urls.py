9417b68a2986e011775a9a5e4cbc266f4c250c5d
Bethany Garcia <bethanymgarcia@gmail.com>
2019-12-25 11:19:54 -0800

    Adjusted urls and moved util file out of app.

--- cbv3_django_prototype/cbv3_django_prototype/userauth/urls.py
+++ cbv3_django_prototype/cbv3_django_prototype/userauth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token
 from .views import current_user, UserList
 
 urlpatterns = [
     path('current_user/', current_user),
-    path('users/', UserList.as_view())
+    path('users/', UserList.as_view()),
+    path('token-auth/', obtain_jwt_token),
 ]
