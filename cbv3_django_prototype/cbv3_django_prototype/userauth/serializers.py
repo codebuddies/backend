23a06e0afb58204600724c2187922132294e81a6
Bethany Garcia <bethanymgarcia@gmail.com>
2020-01-06 21:02:02 -0800

    JWT changes.

--- cbv3_django_prototype/cbv3_django_prototype/userauth/serializers.py
+++ cbv3_django_prototype/cbv3_django_prototype/userauth/serializers.py
@@ -1,24 +1,27 @@
 from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
-from django.contrib.auth.models import User
-
+from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    We use get_user_model here because of the custom User model.
+    See: https://wsvincent.com/django-referencing-the-user-model/.
+    """
 
     class Meta:
-        model = User
-        fields = ('username',)
+        model = get_user_model()
+        fields = ('username', 'is_superuser',)
 
 
 class UserSerializerWithToken(serializers.ModelSerializer):
-    
-    token = serializers.SerializerMethodField()
+
     password = serializers.CharField(write_only=True)
+    token = serializers.SerializerMethodField()
 
     class Meta:
-        model = User
-        fields = ('token', 'username', 'password')
-
+        model = get_user_model()
+        fields = ('username', 'token', 'password', 'first_name', 'last_name', 'email')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def get_token(self, obj):
         jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
@@ -26,12 +29,17 @@ class UserSerializerWithToken(serializers.ModelSerializer):
 
         payload = jwt_payload_handler(obj)
         token = jwt_encode_handler(payload)
+
         return token
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
+
         if password is not None:
             instance.set_password(password)
         instance.save()
+
         return instance
+
+
