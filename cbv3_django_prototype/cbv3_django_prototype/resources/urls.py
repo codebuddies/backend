ff85ef8f95d9a66ac9a28d94bddb75d73f7a546a
Bethany Garcia <bethanymgarcia@gmail.com>
2020-01-06 20:54:24 -0800

    Changed url routes and modified serch_field for media_type.

--- cbv3_django_prototype/cbv3_django_prototype/resources/urls.py
+++ cbv3_django_prototype/cbv3_django_prototype/resources/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 router = routers.DefaultRouter()
