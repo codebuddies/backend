688c2afaa8d572c7f131aadfdf3d94cc62d1bd72
Bethany Garcia <bethanymgarcia@gmail.com>
2020-01-06 20:56:57 -0800

    Added JWT obtain, verify, and refresh endpoints.

--- cbv3_django_prototype/cbv3_django_prototype/userauth/urls.py
+++ cbv3_django_prototype/cbv3_django_prototype/userauth/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
 from .views import current_user, UserList
 
+app_name = "userauth"
+
 urlpatterns = [
+    path('obtain_token/', obtain_jwt_token),
+    path('refresh_token/', refresh_jwt_token),
+    path('validate_token/', verify_jwt_token),
     path('current_user/', current_user),
     path('users/', UserList.as_view()),
-    path('token-auth/', obtain_jwt_token),
 ]
