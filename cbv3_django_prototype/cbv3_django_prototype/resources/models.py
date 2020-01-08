9d65f7942b2210965b7c97e3fcaac3753082339c
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2020-01-07 18:41:41 -0800

    Solves issue 24: rename model fields

--- cbv3_django_prototype/cbv3_django_prototype/resources/models.py
+++ cbv3_django_prototype/cbv3_django_prototype/resources/models.py
@@ -1,16 +1,17 @@
 import uuid
+from taggit.managers import TaggableManager
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-import django.contrib.postgres.fields as pg
 from django.contrib.auth import get_user_model
-
 
 
 def get_sentinel_user():
     return get_user_model().objects.get_or_create(username='deleted')[0]
 
-from taggit.managers import TaggableManager
+
+def get_tags_display(self):
+    return self.tags.values_list('name', flat=True)
 
 
 class Resource(models.Model):
@@ -30,14 +31,14 @@ class Resource(models.Model):
         ('WEB', 'Website')
     ]
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
+    guid = models.UUIDField(default=uuid.uuid4, editable=False)
 
     title = models.CharField(max_length=200)
-    
-    author = models.CharField(max_length=200, blank=True)
+
+    author = models.CharField(blank=True, max_length=200)
 
     # potentially a markdown field.  Will need a markdown converter and renderer
-    description = models.TextField(blank=True, max_length=300)
+    description = models.TextField(blank=True, max_length=600)
 
     # specific URL of resource
     url = models.URLField(max_length=300)
@@ -46,7 +47,7 @@ class Resource(models.Model):
     referring_url = models.URLField(blank=True, max_length=300)
 
     # place or person the user received the recommendation from if not a URL
-    other_referring_source = models.CharField(max_length=200)
+    other_referring_source = models.CharField(blank=True, max_length=200)
 
     # user who posted the resource
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(get_sentinel_user))
@@ -65,12 +66,11 @@ class Resource(models.Model):
 
     paid = models.BooleanField(null=True)
 
-    # JSONB for a simplified DB Schema and prototype for now.
-    #tags = pg.JSONField()
     # Allow tags to be used across entities
     # E.g. so we can create composite views showing all entities sharing a common tag
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
 
     def __str__(self):
         """A string representation of the model."""
         return self.title
+    
