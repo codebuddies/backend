e0b7dc60a61a557d35409791e75afab4a9bff824
Bethany Garcia <bethanymgarcia@gmail.com>
2020-02-29 19:39:45 -0800

    Changed tags field to be through model to CustomTaggedItesm using CustomTaggableManager.  Changed guid field to be UUID1.

--- cbv3_django_prototype/cbv3_django_prototype/resources/models.py
+++ cbv3_django_prototype/cbv3_django_prototype/resources/models.py
@@ -1,9 +1,12 @@
 import uuid
 from taggit.managers import TaggableManager
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth import get_user_model
+from tagging.managers import CustomTaggableManager
+from tagging.models import CustomTag, TaggedItems
+
 
 
 def get_sentinel_user():
@@ -31,7 +34,7 @@ class Resource(models.Model):
         ('WEB', 'Website')
     ]
 
-    guid = models.UUIDField(default=uuid.uuid4, editable=False)
+    guid = models.UUIDField(default=uuid.uuid1, editable=False)
 
     title = models.CharField(max_length=200)
 
@@ -68,7 +71,7 @@ class Resource(models.Model):
 
     # Allow tags to be used across entities
     # E.g. so we can create composite views showing all entities sharing a common tag
-    tags = TaggableManager(blank=True)
+    tags = TaggableManager(through=TaggedItems, manager=CustomTaggableManager, blank=True)
 
     def __str__(self):
         """A string representation of the model."""
