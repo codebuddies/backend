a946cfdd6828f692ba46fe245e64e71f16330dea
chris48s <chris.shaw480@gmail.com>
2020-03-25 20:16:41 +0000

    add a TODO comment for TaggedItemsFactory

--- project/tagging/factories.py
+++ project/tagging/factories.py
@@ -11,3 +11,24 @@ class CustomTagFactory(DjangoModelFactory):
     class Meta:
         model = CustomTag
         django_get_or_create = ["guid"]
+
+
+"""
+TODO:
+I've not tested this because we need a ResourceFactory first but based on the
+https://factoryboy.readthedocs.io/en/latest/recipes.html#django-models-with-genericforeignkeys
+our TaggedItemsFactory is probably going to look roughly like this:
+
+from resources.factories import ResourceFactory
+
+class TaggedItemsFactory(DjangoModelFactory):
+
+    content_type = factory.LazyAttribute(
+        lambda o: ContentType.objects.get_for_model(o.content_object))
+    content_object = factory.SubFactory(ResourceFactory)
+    object_id = factory.SelfAttribute('content_object.id')
+    tag = factory.SubFactory(CustomTagFactory)
+
+    class Meta:
+        model = TaggedItems
+"""
