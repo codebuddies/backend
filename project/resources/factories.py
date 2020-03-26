5aa9bd71a2891c174c7420c00c28c35dc4275285
Maricela Sanchez <mayela0x14@gmail.com>
2020-03-26 17:51:12 -0600

    Add ResourceFactory

--- project/resources/factories.py
+++ project/resources/factories.py
@@ -1,1 +1,40 @@
-# TODO
+import uuid
+from random import choice
+
+from factory import DjangoModelFactory, Faker, LazyAttribute, SubFactory
+
+from users.factories import UserFactory
+from .models import Resource
+
+
+RESOURCE_TYPES = [
+    ('VID', 'Video'),
+    ('POD', 'Podcast'),
+    ('PODEP', 'Podcast Episode'),
+    ('TALK', 'Talk'),
+    ('TUTOR', 'Tutorial'),
+    ('COURSE', 'Course'),
+    ('BOOK', 'Book'),
+    ('BLOG', 'Blog'),
+    ('GAME', 'Game'),
+    ('EVENT', 'Event'),
+    ('TOOL', 'Tool'),
+    ('LIB', 'Library'),
+    ('WEB', 'Website')
+]
+
+
+class ResourceFactory(DjangoModelFactory):
+    guid = LazyAttribute(lambda obj: uuid.uuid1())
+    title = Faker("sentence")
+    author = Faker("name")
+    description = Faker("text")
+    url = Faker("uri")
+    referring_url = Faker("uri")
+    other_rerring_source = Faker("uri")
+    user = SubFactory(UserFactory)
+    media_type = choice(RESOURCE_TYPES)
+    paid = choice(["True", "False"])
+
+    class Meta:
+        model = Resource
