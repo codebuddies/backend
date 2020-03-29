9e3732f13c5262cf97f9ea3cd86450c4fbdc847c
chris48s <chris.shaw480@gmail.com>
2020-03-29 14:05:27 +0100

    WIP add tests for tag slugging

--- project/tagging/tests.py
+++ project/tagging/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .models import CustomTag
 
-# Create your tests here.
+
+class CustomTagTests(TestCase):
+    def test_slugging(self):
+        test_tags = [
+            {"name": "programming", "expected_slug": "programming"},
+            {"name": "PyCon", "expected_slug": "pycon"},
+            {"name": "local storage", "expected_slug": "local-storage"},
+            {"name": "PEN testing", "expected_slug": "pen-testing"},
+            {"name": "תִּיכְנוּת", "expected_slug": "תיכנות"},
+            {"name": " 프로그램 작성", "expected_slug": "프로그램-작성"},
+            {"name": "程式设计", "expected_slug": "程式设计"},
+            {"name": "برمجة", "expected_slug": "برمجة"},
+            {"name": "आनंद", "expected_slug": "आनद"},
+            {"name": "лягушачий", "expected_slug": "лягушачий"},
+            {"name": "教程", "expected_slug": "教程"},
+            {"name": "Inicio r\u00e1pido", "expected_slug": "inicio-r\u00e1pido"},
+            {"name": "最后", "expected_slug": "最后"},
+            {"name": " 欲求不満", "expected_slug": "欲求不満"},
+            {"name": "စမ်းသပ်ခြင်း", "expected_slug": "စမသပခင"},
+            {"name": "ฐานข้อมูล", "expected_slug": "ฐานขอมล"},
+            {"name": "основы", "expected_slug": "основы"},
+            {"name": "אַלגערידאַם", "expected_slug": "אלגערידאם"},
+            {"name": "自動化する", "expected_slug": "自動化する"},
+            {"name": "sjálfvirkan", "expected_slug": "sjálfvirkan"},
+            {"name": "پژوهش ", "expected_slug": "پژوهش"},
+            {"name": " గ్రాఫ్", "expected_slug": "గరఫ"},
+            {"name": "데이터 베이스", "expected_slug": "데이터-베이스"},
+
+            {"name": "हिंदी में जानकारी", "expected_slug": "-जनकर"},
+            {"name": "प्रयास है", "expected_slug": "रयस-ह"},
+            {"name": "stòran-dàta", "expected_slug": "sstòran-dàta"},
+            {"name": "స్వయంచాలకం", "expected_slug": "యచలక"},
+
+            {"name": "❤", "expected_slug": ""},
+            {"name": "🐸", "expected_slug": "_1"},
+        ]
+
+        for tag in test_tags:
+            t = CustomTag(name=tag["name"])
+            t.save()
+            self.assertEqual(t.slug, tag["expected_slug"])
