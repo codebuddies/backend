c66843f231b085e7249c48f198cf1bc4bf51b683
Bethany Garcia <bethanymgarcia@gmail.com>
2020-05-18 01:06:02 -0700

    Changed validation to raise a serialization.ValidationError.  Added checks for disallowed characters in tags.

--- project/tagging/serializers.py
+++ project/tagging/serializers.py
@@ -1,32 +1,59 @@
+import regex
 from rest_framework import serializers
-
+from .models import CustomTag
 
 
 class TagsSerializerField(serializers.ModelField):
 
     child = serializers.CharField()
+
     default_error_messages = {
-            'not_a_list': ('Expected a list of tag names but got type "{input_type}".'),
-            'not_a_string': ('All list items must be type str.')
+            'not_a_list': ('Expected a list of tag names but got type {input_type}.'),
+            'not_a_string': ('All tags must be of type str.'),
+            'emoji or symbol': ('Emoji, pictograps, and symbols are not supported in tags.')
                     }
+
+    class Meta:
+        model = CustomTag
+        fields = ('name',)
 
     def __init__(self, **kwargs):
         super(TagsSerializerField, self).__init__(**kwargs)
 
-    def to_internal_value(self, value):
+    def validate(self, value):
         if not value:
             value = []
 
         if not isinstance(value, list):
-            self.fail('not_a_list', input_type=type(value).__name__)
+            raise serializers.ValidationError(self.fail('not_a_list', input_type=type(value).__name__))
 
         for tag in value:
             if not isinstance(tag, str):
-                self.fail('not_a_string')
+                raise serializers.ValidationError(self.fail('not_a_string'))
+
+            if self.unsupported_characters(tag):
+                raise serializers.ValidationError(self.fail('emoji or symbol'))
 
             self.child.run_validation(tag)
 
         return value
+
+    def unsupported_characters(self, tag):
+        '''
+        Everything NOT a letter, whitespace  or combining mark in any unicode language
+        additionally, miscellaneous mathematical and general symbols are also not allowed.
+        '''
+
+        disallowed_characters = regex.compile(
+            u"([\p{InMiscellaneous_Mathematical_Symbols-A}"
+            u"\p{InMiscellaneous_Mathematical_Symbols-B}\p{So}]+)"
+            u"|([^\p{L}\p{M}\p{Z}\p{N}\p{P}]+)",
+            regex.VERBOSE)
+
+        return True if regex.search(disallowed_characters, tag) else False
+
+    def to_internal_value(self, value):
+        return self.validate(value)
 
     def to_representation(self, instance):
 
