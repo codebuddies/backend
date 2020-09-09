cd5b0c0dbf796bffa8b2299effb96e2063d45e34
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2020-09-08 23:13:11 -0700

    Started writing tests for the osprojects model

--- project/osprojects/tests.py
+++ project/osprojects/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import pytest
+from .factories import OSProjectsFactory
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_osprojects_model():
+    """ Test OSProjects model """
+    # create OSProjects model instance
+    osproject = OSProjectsFactory(title="Buddybot", guid="cc63f918-d515-11ea-956f-0242ac130002")
+    assert osproject.title == "Buddybot"
+    assert osproject.guid == "cc63f918-d515-11ea-956f-0242ac130002"
