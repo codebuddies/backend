d7e5e1b33e84b6b2f9d67b12facfdcdbdd0d1a8b
BethanyG <bethanymgarcia@gmail.com>
2020-09-21 20:51:18 -0700

    Added confirm_email method.  Will probably remove it as uneeded later.

--- project/userauth/adapter.py
+++ project/userauth/adapter.py
@@ -1,4 +1,5 @@
 from allauth.account.adapter import DefaultAccountAdapter
+from django.contrib.sites.models import Site
 from allauth.utils import build_absolute_uri
 from django.conf import settings
 
@@ -8,3 +9,11 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         url = settings.CUSTOM_ACCOUNT_CONFIRM_EMAIL_URL.format(emailconfirmation.key)
         result = build_absolute_uri(request, url)
         return result
+
+    def confirm_email(self, request, email_address):
+        """
+        Marks the email address as confirmed on the db
+        """
+        email_address.verified = True
+        email_address.set_as_primary(conditional=True)
+        email_address.save()
